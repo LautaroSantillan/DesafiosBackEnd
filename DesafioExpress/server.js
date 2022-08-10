b1570baeb26d09aa161a005ffbc7f04a77921c17
lautarosantillan <santillanlautaro03@gmail.com>
2022-08-10 01:50:39 -0300

    Ultimos retoques

--- DesafioExpress/server.js
+++ DesafioExpress/server.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const Contenedor = require('./contenedor');
+const express = require("express");
+const Contenedor = require("./contenedor");
 const app = express();
 const PORT = 8080;
 const productos = new Contenedor();
 
-app.get('/productos', async (req, res) => {
+app.get("/", async (req, res) => {
+    res.send('<h1 style="color: blue; text-align: center">Servidor Inicializado</h1><br><h2>Vaya a http://localhost:8080/productos y http://localhost:8080/productoRandom para obtener los resultados</h2>');
+});
+
+app.get("/productos", async (req, res) => {
     const products = await productos.getAll();
     res.send(products);
 });
 
-app.get('/productoRandom', async (req, res) => {
+app.get("/productoRandom", async (req, res) => {
     const product = await productos.getRandom();
     res.send(product);
 });
 
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
     res.send('<h1 style="color: red;">ERROR 404 - PAGE NOT FOUND</h1>');
 });
 
 const server = app.listen(PORT, () => {
-    console.log(`El servidor HTTP se esta escuchando correctamente en http://localhost:${PORT}/productos y en http://localhost:${PORT}/productoRandom`);
+    console.log(`El servidor HTTP se esta escuchando correctamente en http://localhost:${PORT}/`);
 });
