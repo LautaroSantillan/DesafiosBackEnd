caf82190d2e1e0be44b21d1fb50c6f99d0e7930e
lautarosantillan <santillanlautaro03@gmail.com>
2022-08-17 12:38:06 -0300

    Reorganizacion del desafio

---
+++ DesafioRESTful/productos.routes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const routerProductos = express.Router();
+const Container = require('./class/contenedor');
+const productos = new Container('./data/productos.json');
+
+routerProductos.get('/', (req, res)=>{
+    return res.json(productos.content);
+});
+
+routerProductos.get('/:id', (req, res)=>{
+    let id = Number(req.params.id);
+    return res.json(productos.getById(id));
+});
+
+routerProductos.post('/', (req, res)=>{
+    let obj = req.body;
+    return res.json(productos.save(obj));
+})
+
+routerProductos.put("/:id", (req, res) => {
+    let obj = req.body;
+    let id = Number(req.params.id);
+    return res.json(productos.update(id, obj));
+});
+
+routerProductos.delete("/:id", (req, res) => {
+    let id = Number(req.params.id);
+    return res.json(productos.deleteById(id));
+});
+
+module.exports = routerProductos;
